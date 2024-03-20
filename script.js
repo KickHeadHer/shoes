4751474e3089d805dba3806098a299de6f5a10b2
KickHeadHer <164255774+KickHeadHer@users.noreply.github.com>
2024-03-21 03:48:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function buy(productName, price) {
         alert(`Your purchase of ${productName} for \u20B9${price.toFixed(2)} has been confirmed! Thank you for shopping at KickHeadHer.`);
     }
 }
+
+function addToCart(productName, price) {
+    var confirmation = confirm(`Add ${productName} to cart for ₹${price.toFixed(2)}?`);
+    if (confirmation) {
+        alert(`${productName} added to cart!`);
+    }
+}
